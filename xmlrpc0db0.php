ae15e21bc157d011f23ccb4735f3ac36b68d945b
leandro alves de sousa <71714626+leandro0202@users.noreply.github.com>
2023-06-27 19:09:40 -0300

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://cursonailacademy.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cursonailacademy.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cursonailacademy.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cursonailacademy.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cursonailacademy.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cursonailacademy.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
